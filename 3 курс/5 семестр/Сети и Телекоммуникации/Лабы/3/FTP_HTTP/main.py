20e61a3e3d74fbde3758766cca18f9becd5f568e
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-10-25 04:46:43 +0700

    error processing

--- 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/main.py
+++ 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/main.py
@@ -8,19 +8,42 @@ CMD_PASS = b"PASS password\r\n"
 CMD_PASV = b"PASV\r\n"
 CMD_LIST = b"LIST\r\n"
 CMD_QUIT = b"QUIT\r\n"
-
+CMD_CWD = b"CDUP\r\n"
+CMD_HELP = b"HELP\r\n"
+CMD_RETR = b"RETR readme.txt\r\r"
 
 def send_command(command: bytes):
     time.sleep(0.1)
-    print(command.decode("utf-8"))
+    print("Client: ", command.decode("utf-8"), end='')
     socket_manager.send(command)
-    buffer = socket_manager.recv(1024).decode("utf-8")
-    print(buffer)
     while True:
+        buffer = socket_manager.recv(1024).decode("utf-8")
+
         if buffer[:4] == "xyz-":
+            print(buffer)
             buffer = socket_manager.recv(1024).decode("utf-8")
             print(buffer)
         else:
+            match buffer[0]:
+                case '2':
+                    print("Server: Successful response: ", buffer, end='')
+                case '4' | '5':
+                    print("Server: Command cannot be executed: ", buffer, end='')
+                case '1' | '3':
+                    print("Server: Error or incomplete answer: ", buffer, end='')
+            match buffer[1]:
+                case '0':
+                    print("Response type: Syntactic")
+                case '1':
+                    print("Response type: Informational. Corresponds to the informational message.")
+                case '2':
+                    print("Response type: Compound. The message refers to either a control connection or a data connection.")
+                case '3':
+                    print("Response type: Corresponds to messages about user authentication and rights.")
+                case '4':
+                    print("Response type: Undefined.")
+                case '5':
+                    print("Response type: File system. Corresponds to a file system status message.")
             break
     return buffer
 
@@ -29,12 +52,10 @@ if __name__ == '__main__':
     socket_manager = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
     try:
         socket_manager.connect(("test.rebex.net", 21))
-        print("Connection OK")
+        buffer_init = socket_manager.recv(1024).decode("utf-8")
+        print(buffer_init)
     except:
         print("Error connection")
-
-    buffer_init = socket_manager.recv(1024).decode("utf-8")
-    print(buffer_init)
 
     # Имя пользователя для входа на сервер.
     send_command(CMD_USER)
@@ -57,11 +78,23 @@ if __name__ == '__main__':
     except:
         print("Error connection")
     # после команды LIST возвращается список файлов каталога. Список передаётся через соединение данных.
-    list_data = send_command(CMD_LIST)
-    buf = socket_data.recv(1024).decode("utf-8")
-    print(buf)
+    send_command(CMD_LIST)
+    list_data = socket_data.recv(1024).decode("utf-8")
+    print(list_data)
+
+    time.sleep(0.1)
+    print("Client: ", CMD_RETR.decode("utf-8"), end='')
+    socket_manager.send(CMD_RETR)
+    print(socket_manager.recv(1024))
+
+    # send_command(CMD_HELP)
+    # help = socket_data.recv(1024).decode("utf-8")
+    # print(help)
+
+    # send_command(CMD_CWD)
+    # send_command(CMD_LIST)
+    # list_data = socket_data.recv(1024).decode("utf-8")
+    # print(list_data)
+
     send_command(CMD_QUIT)
     socket_manager.close()
-
-
-
