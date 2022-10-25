c8ab7cb644c8a16cb496e28b1aad28174cb01058
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-10-25 16:25:21 +0700

    handler multy recv (xyz-)

--- 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/gui.py
+++ 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/gui.py
@@ -16,11 +16,21 @@ CMD_RETR = b"RETR readme.txt\r\r"
 
 
 ####################################### Stage #1: INITIALIZATION SERVER  ###############################################
+def recv_all(socket_manager):
+    answer = socket_manager.recv(1024).decode('utf-8')
+    print(answer, end='')
+    while answer[3] == '-':
+        answer = socket_manager.recv(1024).decode('utf-8')
+        print(answer, end='')
+    return answer
+
+
 def init_server(sender, app_data, user_data):
     socket_manager = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
     try:
         socket_manager.connect((dpg.get_value(ftp_server_id), dpg.get_value(ftp_port_id)))
-        buffer_init = socket_manager.recv(1024).decode("utf-8")
+        buffer_init = recv_all(socket_manager)
+        #buffer_init = socket_manager.recv(1024).decode("utf-8")
         dpg.add_text(buffer_init[4:], tag='connect', before=sep)
     except:
         dpg.add_text("Incorrect Server or PORT", tag='err', before=sep)
@@ -36,7 +46,8 @@ def init_user(socket_manager):
     CMD_PASS = f"PASS {dpg.get_value(password_id)}\r\n".encode("utf-8")
     # Имя пользователя для входа на сервер.
     socket_manager.send(CMD_USER)
-    print(socket_manager.recv(1024).decode('utf-8'), end='')
+    recv_all(socket_manager)
+    #print(socket_manager.recv(1024).decode('utf-8'), end='')
     # Пароль пользователя для входа на сервер.
     socket_manager.send(CMD_PASS)
     response_handler(socket_manager)
@@ -45,8 +56,13 @@ def init_user(socket_manager):
 
 ############################################# Stahe #3: RESPONSE HANDLER ###############################################
 def response_handler(socket_manager):
-    answer = socket_manager.recv(1024).decode('utf-8')
-    print(answer, end='')
+    answer = recv_all(socket_manager)
+    # answer = socket_manager.recv(1024).decode('utf-8')
+    # print(answer)
+    # while answer[3] == '-':
+    #     answer = socket_manager.recv(1024).decode('utf-8')
+    #     print(answer, end='')
+
     match answer[0]:
         case '2':
             dpg.add_text("Connection successful.", tag='success', before="answers")
@@ -63,11 +79,11 @@ def response_handler(socket_manager):
 
 ############################################# AUTHORIZATION ############################################################
 with dpg.window(label="AUTHORIZATION", modal=True, show=False, tag="auth", no_title_bar=True, autosize=True, ):
-    ftp_server_id = dpg.add_input_text(label="Server", default_value="test.rebex.net")
+    ftp_server_id = dpg.add_input_text(label="Server", default_value="ftp.gnu.org")
     ftp_port_id = dpg.add_input_int(label="PORT", default_value=DEFAULT_PORT)
     sep = dpg.add_separator()
-    login_id = dpg.add_input_text(label="login", default_value="demo")
-    password_id = dpg.add_input_text(label="password", default_value="password", password=True)
+    login_id = dpg.add_input_text(label="login", default_value="anonymous")
+    password_id = dpg.add_input_text(label="password", default_value="anonymous", password=True)
     with dpg.group(horizontal=True, tag="answers"):
         dpg.add_button(label="Save", width=75, callback=init_server)
         dpg.add_button(label="Cancel", width=75, callback=lambda: dpg.configure_item("auth", show=False))
