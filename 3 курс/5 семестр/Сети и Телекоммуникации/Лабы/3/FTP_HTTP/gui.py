391c8ffadaf56f67be5c800a0b65096ba5a135e4
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-10-29 02:26:56 +0700

    SiT (3lt - FTP)
    pasv in ftp

--- 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/gui.py
+++ 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/gui.py
@@ -3,6 +3,9 @@ from socket import *
 import re
 import time
 
+DEFAULT_SERVER = "ftp.slackware.com"
+DEFAULT_LOGIN = "anonymous"
+DEFAULT_PASS = "password"
 DEFAULT_PORT = 21
 
 dpg.create_context()
@@ -18,12 +21,10 @@ CMD_RETR = b"RETR readme.txt\r\r"
 ####################################### Stage #1: INITIALIZATION SERVER  ###############################################
 def recv_all(socket_manager):
     answer = socket_manager.recv(1024).decode('utf-8')
-    print("1: ", answer, end='')
-    count = 2
+    print(answer, end='')
     while not re.search(r'\d{3} ', answer):
         answer = socket_manager.recv(1024).decode('utf-8')
-        print(count, answer, end='')
-        count += 1
+        print(answer, end='')
     return answer
 
 
@@ -37,8 +38,11 @@ def init_server(sender, app_data, user_data):
         dpg.add_text("Incorrect Server or PORT", tag='err', before=sep)
         time.sleep(2.0)
         dpg.delete_item("err")
+        socket_manager.close()
         return
     init_user(socket_manager)
+    socket_data = init_pasv(socket_manager)
+    response_handler("220" if socket_data else "520", socket_data)
 
 
 ############################################ Stage #2: INITIALIZATION USER #############################################
@@ -51,13 +55,38 @@ def init_user(socket_manager):
 
     # Пароль пользователя для входа на сервер.
     socket_manager.send(CMD_PASS)
-    response_handler(socket_manager)
+    answer = recv_all(socket_manager)
     dpg.delete_item("connect")
 
 
+def init_pasv(socket_manager):
+    #  Войти в пассивный режим. Сервер вернёт адрес и порт, к которому нужно подключиться, чтобы забрать данные
+    socket_manager.send(CMD_PASV)
+    pasv = recv_all(socket_manager)
+    # ищем подстроку соответствующую регулярному выражению
+    match = re.search(r"(\d+,\d+,\d+,\d+,\d+,\d+)", pasv)
+    # разбиваем через запятую получая список чисел соответствующих ip port
+    match = re.split(r",", match[0])
+    ip = ".".join(match[0:4])
+    port = int(match[4]) * 256 + int(match[5])
+    print(f"ip {ip} port {port}")
+
+    socket_data = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
+    try:
+        socket_data.connect((ip, port))
+        dpg.add_text("Connection OK", tag='socket_data', before='answers')
+        time.sleep(2.0)
+        dpg.delete_item("socket_data")
+        return socket_data
+    except:
+        dpg.add_text("Discard Connection", tag='socket_data', before='answers')
+        time.sleep(2.0)
+        dpg.delete_item("socket_data")
+        socket_data.close()
+
+
 ############################################# Stahe #3: RESPONSE HANDLER ###############################################
-def response_handler(socket_manager):
-    answer = recv_all(socket_manager)
+def response_handler(answer, socket):
     match answer[0]:
         case '2':
             dpg.add_text("Connection successful.", tag='success', before="answers")
@@ -68,17 +97,17 @@ def response_handler(socket_manager):
             dpg.add_text("Incorrect login or password.", tag='incorrect', before="answers")
             time.sleep(2.0)
             dpg.delete_item("incorrect")
-            socket_manager.close()
+            socket.close()
 ################################################ END AUTHORIZATION #####################################################
 
 
 ############################################# AUTHORIZATION ############################################################
 with dpg.window(label="AUTHORIZATION", modal=True, show=False, tag="auth", no_title_bar=True, autosize=True, ):
-    ftp_server_id = dpg.add_input_text(label="Server", default_value="ftp.gnu.org")
+    ftp_server_id = dpg.add_input_text(label="Server", default_value=DEFAULT_SERVER)
     ftp_port_id = dpg.add_input_int(label="PORT", default_value=DEFAULT_PORT)
     sep = dpg.add_separator()
-    login_id = dpg.add_input_text(label="login", default_value="anonymous")
-    password_id = dpg.add_input_text(label="password", default_value="anonymous", password=True)
+    login_id = dpg.add_input_text(label="login", default_value=DEFAULT_LOGIN)
+    password_id = dpg.add_input_text(label="password", default_value=DEFAULT_PASS, password=True)
     with dpg.group(horizontal=True, tag="answers"):
         dpg.add_button(label="Save", width=75, callback=init_server)
         dpg.add_button(label="Cancel", width=75, callback=lambda: dpg.configure_item("auth", show=False))
--- 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/main.py
+++ 3 курс/5 семестр/Сети и Телекоммуникации/Лабы/3/FTP_HTTP/main.py
@@ -6,22 +6,7 @@ if __name__ == '__main__':
     send_command(CMD_USER)
 
     send_command(CMD_PASS)
-    #  Войти в пассивный режим. Сервер вернёт адрес и порт, к которому нужно подключиться, чтобы забрать данные
-    ret = send_command(CMD_PASV)
-    # ищем подстроку соответствующую регулярному выражению
-    match = re.search(r"(\d+,\d+,\d+,\d+,\d+,\d+)", ret)
-    # разбиваем через запятую получая список чисел соответствующих ip port
-    match = re.split(r",", match[0])
-    ip = ".".join(match[0:4])
-    port = int(match[4]) * 256 + int(match[5])
-    print(f"ip {ip} port {port}")
 
-    socket_data = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-    try:
-        socket_data.connect((ip, port))
-        print("Connection OK")
-    except:
-        print("Error connection")
     # после команды LIST возвращается список файлов каталога. Список передаётся через соединение данных.
     send_command(CMD_LIST)
     list_data = socket_data.recv(1024).decode("utf-8")
