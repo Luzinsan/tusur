0bad8da7b189a059dd8f10bf237ae84725416406
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-12-09 20:40:05 +0700

    ТСиСА (Курсач): рефакторинг

--- 3_курс/5_семестр/ТСиСА/Курсовая/приложение/main.py
+++ 3_курс/5_семестр/ТСиСА/Курсовая/приложение/main.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
 import numpy as np
+
 dpg.create_context()
 
 
@@ -13,20 +14,12 @@ estimates = np.array([1, 1, 1])
 def switch_expert(sender, app_data, expert_data):
     # expert_data = [number_of_expert, count_of_alternatives, count_of_experts]
     global estimates
-    for i in range(expert_data[1]):
-        for j in range(expert_data[1]):
-            temp = dpg.get_value(f'mark{expert_data[0]}{i}{j}')
-            opposite_temp = dpg.get_value(f'mark{expert_data[0]}{j}{i}')
-            if (i == j) and (temp == 1) \
-                    or (temp == (not opposite_temp)):
-                estimates[expert_data[0]][i][j] = temp
-            else:
-                dpg.add_text(f'Неверно введены данные. Элемент: строка:{i+1};\tстолбец{j+1}',
-                             parent=f'evaluation{expert_data[0]}')
-                return False
+    estimates[expert_data[0]] = [[dpg.get_value(f'mark{expert_data[0]}{i}{j}')
+                                 for j in range(expert_data[1])]
+                                 for i in range(expert_data[1])]
     dpg.configure_item(f'evaluation{expert_data[0]}', show=False)
     if expert_data[0] + 1 != expert_data[2]:
-        dpg.configure_item(f'evaluation{expert_data[0]+1}', show=True)
+        dpg.configure_item(f'evaluation{expert_data[0] + 1}', show=True)
     else:
         ranging()
 
@@ -42,11 +35,10 @@ def ranging():
             for expert in range(count_expert):
                 sum_marks += estimates[expert][i][j]
             common_matrix[i][j] = True if sum_marks > count_expert / 2 else False
-    print(common_matrix)
     with dpg.child_window(tag=f'range_window', parent='expert_window', height=720, width=1920):
         for expert in range(count_expert):
             with dpg.group(horizontal=True, tag=f"group_expert{expert}"):
-                dpg.add_text(default_value=f"Эксперт #{expert+1}: ")
+                dpg.add_text(default_value=f"Эксперт #{expert + 1}: ")
                 dpg.add_input_text(default_value=dpg.get_value(f'role{expert}'), readonly=True)
         # проранжированные альтернативы
         with dpg.table(tag=f'range_table',
@@ -60,13 +52,16 @@ def ranging():
             for i in range(0, count_alternatives):
                 with dpg.table_row():
                     dpg.add_text(default_value=dpg.get_value(f'alter_text{i}'), tag=f'alter_row{i}')
-                    calc_marks[i] = count_alternatives-sum(common_matrix[i])+1
+                    calc_marks[i] = count_alternatives - sum(common_matrix[i]) + 1
                     dpg.add_text(tag=f'common_range{i}', default_value=calc_marks[i])
-
-        print(calc_marks)
         with dpg.group(horizontal=True):
             dpg.add_text(default_value="Наилучшая альтернатива: ")
             dpg.add_input_text(default_value=dpg.get_value(f'alter_text{np.argmin(calc_marks)}'), readonly=True)
+
+
+def check_mark(sender, checked_mark, reflected_mark):
+    if checked_mark == dpg.get_value(f'mark{reflected_mark[0]}{reflected_mark[1]}{reflected_mark[2]}'):
+        dpg.set_value(f'mark{reflected_mark[0]}{reflected_mark[1]}{reflected_mark[2]}', not checked_mark)
 
 
 def experts():
@@ -74,43 +69,47 @@ def experts():
     dpg.configure_item('expert_window', show=True)
     with dpg.group(horizontal=True, tag="group_target_const", parent='expert_window'):
         dpg.add_text(default_value=f"ЦЕЛЬ: ")
-        dpg.add_input_text(readonly=True, default_value=dpg.get_value('target'))
+        dpg.add_input_text(readonly=True, default_value=dpg.get_value('target'), width=1900)
     count_expert = dpg.get_value('experts')
     count_alternatives = dpg.get_value('alternatives')
     alternatives = []
-    for i in range(count_alternatives):
-        alternatives.append(dpg.get_value(f'alter_text{i}'))
+    for expert in range(count_alternatives):
+        alternatives.append(dpg.get_value(f'alter_text{expert}'))
 
     dpg.add_listbox(tag='list_alternatives', items=alternatives, parent='expert_window',
                     tracked=True, width=1920, num_items=count_alternatives)
     estimates.resize([count_expert, count_alternatives, count_alternatives])
-    for i in range(count_expert):
-        with dpg.child_window(tag=f'evaluation{i}', parent='expert_window', height=720, width=1920, show=False):
-            with dpg.group(horizontal=True, tag=f"group_role{i}"):
-                dpg.add_text(default_value=f"Роль эксперта #{i+1}: ")
-                dpg.add_input_text(tag=f'role{i}')
+    for expert in range(count_expert):
+        with dpg.child_window(tag=f'evaluation{expert}', parent='expert_window', height=720, width=1920, show=False):
+            with dpg.group(horizontal=True, tag=f"group_role{expert}"):
+                dpg.add_text(default_value=f"Роль эксперта #{expert + 1}: ")
+                dpg.add_input_text(tag=f'role{expert}')
             # табличка с альтернативами
-            with dpg.table(tag=f'table{i}',
+            with dpg.table(tag=f'table{expert}',
                            header_row=True, row_background=True,
                            resizable=True, policy=dpg.mvTable_SizingStretchProp,
                            borders_innerH=True, borders_outerH=True, borders_innerV=True,
                            borders_outerV=True):
-                dpg.add_table_column(label=' ', tag=f'col{i}')
-                for j in range(0, count_alternatives):
-                    dpg.add_table_column(label=f'Alternative #{j+1}', tag=f'col{i}{j}')
-                for j in range(0, count_alternatives):
+                dpg.add_table_column(label=' ', tag=f'col{expert}')
+                for row in range(0, count_alternatives):
+                    dpg.add_table_column(label=f'Alternative #{row + 1}', tag=f'col{expert}{row}')
+                for row in range(0, count_alternatives):
                     with dpg.table_row():
-                        dpg.add_text(default_value=f'Alternative #{j+1}', tag=f'row{i}{j}')
-                        for k in range(0, count_alternatives):
+                        dpg.add_text(default_value=f'Alternative #{row + 1}', tag=f'row{expert}{row}')
+                        for col in range(0, count_alternatives):
                             default_value = 0
-                            if k >= j:
+                            if col > row:
                                 default_value = 1
-                            dpg.add_input_int(tag=f'mark{i}{j}{k}', default_value=default_value,
-                                              min_value=0, max_value=1,
-                                              min_clamped=True, max_clamped=True)
+                            if col == row:
+                                dpg.add_input_int(tag=f'mark{expert}{row}{col}', default_value=1, readonly=True)
+                            else:
+                                dpg.add_input_int(tag=f'mark{expert}{row}{col}', default_value=default_value,
+                                                  min_value=0, max_value=1,
+                                                  min_clamped=True, max_clamped=True,
+                                                  callback=check_mark, user_data=[expert, col, row])
             # Переход к другому эксперту
-            dpg.add_button(label=f"Перейти к эксперту #{i + 1}", tag=f'next{i+1}',
-                           callback=switch_expert, user_data=[i, count_alternatives, count_expert])
+            dpg.add_button(label=f"Перейти к эксперту #{expert + 1}", tag=f'next{expert + 1}',
+                           callback=switch_expert, user_data=[expert, count_alternatives, count_expert])
     dpg.configure_item('evaluation0', show=True)
 
 
@@ -125,7 +124,6 @@ def add_alternatives():
 with dpg.window(label="Expert", tag="expert_window", autosize=True, show=False, modal=True, width=1920):
     dpg.add_separator()
 
-
 with dpg.window(label="Main", tag="Main", autosize=True):
     with dpg.group(horizontal=True, tag="group_target"):
         dpg.add_text(default_value='Введите рассматриваемую цель: ')
@@ -136,11 +134,9 @@ with dpg.window(label="Main", tag="Main", autosize=True):
     with dpg.group(horizontal=True, tag="group_alternatives"):
         dpg.add_text(default_value='Выберите количество альтернатив: ')
         dpg.add_input_int(label=" ", tag='alternatives', default_value=5)
-
     dpg.add_button(label="Ввести альтернативы", width=300, callback=add_alternatives)
     dpg.add_child_window(tag='alternatives_window', height=500)
     dpg.add_button(label="Продолжить", tag='expert_button', width=150, callback=experts)
-
 
 with dpg.theme() as global_theme:
     with dpg.theme_component(dpg.mvAll):
@@ -160,7 +156,7 @@ dpg.bind_font("Default font")
 
 dpg.bind_theme(global_theme)
 ########################################################################################################################
-dpg.create_viewport(title='DECISION MAKING', width=1920, height=920)
+dpg.create_viewport(title='ГРУППОВОЕ ПАРНОЕ ОЦЕНИВАНИЕ', width=1920, height=920)
 dpg.set_global_font_scale(1.25)
 dpg.set_exit_callback(on_exit)
 dpg.setup_dearpygui()
