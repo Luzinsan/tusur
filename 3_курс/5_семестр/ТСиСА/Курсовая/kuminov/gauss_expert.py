947703a59a1b50ceb22959028b65ea7c9e775bc9
DarkMessiah5 <pavekum@mail.ru>
2022-12-11 10:54:57 +0300

    Add files via upload

--- 3_курс/5_семестр/ТСиСА/Курсовая/kuminov/gauss_expert.py
+++ 3_курс/5_семестр/ТСиСА/Курсовая/kuminov/gauss_expert.py
@@ -1,9 +1,9 @@
-
+import math
 
 class GaussExpert:
-    __crits = 0
-    __alts = 0
-    grades = [[]] * 1
+    __crits = 2
+    __alts = 2
+    grades = [[], []]
     gauss_parametrs = [{'dominant_value': [0.0] * 3,
                         'bound_neighb': [0.0] * 2,
                         'membership_deg': [0.0] * 2,
@@ -17,7 +17,45 @@ class GaussExpert:
                        ]
 
     def __init__(self):
+        self.__alts = 5
 
+        self.grades = [[0] * self.__alts for i in range(2)]
+        print(self.grades)
+
+        for i in range(self.__crits):
+            for j in range(3):
+                self.gauss_parametrs[i]['dominant_value'][j] = float(input(f'crit#{i} dv{j}: '))
+            for j in range(2):
+                self.gauss_parametrs[i]['bound_neighb'][j] = float(input(f'crit#{i} bn{j}: '))
+            for j in range(2):
+                self.gauss_parametrs[i]['membership_deg'][j] = float(input(f'crit#{i} md{j}: '))
+
+
+        for j in range(self.__crits):
+            for i in range(self.__alts):
+                self.grades[j][i] = int(input(f'Введите альт#{j} крит#{i}: '))
+
+        print(self.grades)
+
+
+    #GaussExpert(int alts_amount, int, crit_amount,
+    #                     gauss_parametrs *params_, double **grades_) { // АРГУМЕНТ ДОЛЖЕН БЫТЬ КОНСТАНТНЫМ!!!
+    #             crits = crit_amount;
+    #             alts = alts_amount;
+    #             params = std::move(params_);
+    #             grades = std::move(grades_);
+    #
+    #             calculateParams();
+    #
+    #             for (int i = 0; i < crits; i++) {
+    #                 for (int j = 0; j < 4; j++) {
+    #                     std::cout << "Sigma: " << params[i].ds_sigma[j] << std::endl;
+    #                 }
+    #                 for (int j = 0; j < 4; j++) {
+    #                     std::cout << "Y: " << params[i].y[j] << std::endl;
+    #                 }
+    #             }
+    #         }
 
     def calculateParams(self):
         for i in range(self.__crits):
@@ -31,183 +69,143 @@ class GaussExpert:
                         md_ind = 1
                     case 3:
                         dv_ind = 2
+                self.gauss_parametrs[i]['ds_sigma'][j] = ((self.gauss_parametrs[i]['bound_neighb'][bn_ind] -
+                                                          self.gauss_parametrs[i]['dominant_value'][dv_ind]) ** 2) \
+                                                          / (-math.log(self.gauss_parametrs[i]['membership_deg'][md_ind]))
 
-                params[i].ds_sigma[j] = pow(params[i].bound_neighb[bn_ind] -
-                                        params[i].dominant_value[dv_ind], 2) /
-                                        (-std::log(params[i].membership_deg[md_ind]));
-                params[i].y[j] = params[i].dominant_value[dv_ind] + pow(-1, j) *
-                                 abs(sqrt(-params[i].ds_sigma[j] * std::log(0.05)));
+                self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][dv_ind] + (-1) ** j * \
+                                                  math.fabs((-self.gauss_parametrs[i]['ds_sigma'][j] * math.log(0.05)) ** 0.5)
 
-                if (params[i].y[j] > params[i].dominant_value[2])
-                    params[i].y[j] = params[i].dominant_value[2];
-                if (params[i].y[j] < params[i].dominant_value[0])
-                    params[i].y[j] = params[i].dominant_value[0];
-            }
-        }
-    }
-#
-#         double gaussEstimate(int crit, int alt, int membership) {
-#             if (membership == 0) {
-#                 if (grades[crit][alt] < params[crit].dominant_value[0])
-#                     return 1;
-#
-#                 if (grades[crit][alt] > params[crit].y[0])
-#                     return 0;
-#
-#                 return exp(-pow(grades[crit][alt]
-#                                 - params[crit].dominant_value[0], 2)
-#                             / params[crit].ds_sigma[0]);
-#             }
-#
-#             if (membership == 1) {
-#                 if (params[crit].y[1] <= grades[crit][alt] &&
-#                     grades[crit][alt] >= params[crit].y[2])
-#                     return 0;
-#
-#                 if (grades[crit][alt] > params[crit].y[1] &&
-#                     grades[crit][alt] < params[crit].dominant_value[1])
-#                     return exp(-pow(grades[crit][alt]
-#                                     - params[crit].dominant_value[1], 2)
-#                                 / params[crit].ds_sigma[1]);
-#
-#                 return exp(-pow(grades[crit][alt]
-#                                 - params[crit].dominant_value[1], 2)
-#                             / params[crit].ds_sigma[2]);
-#             }
-#
-#             if (membership == 2) {
-#                 if (grades[crit][alt] <= params[crit].y[3])
-#                     return 0;
-#
-#                 if (grades[crit][alt] >= params[crit].dominant_value[2])
-#                     return 1;
-#
-#                 return exp(-pow(grades[crit][alt]
-#                                 - params[crit].dominant_value[2], 2)
-#                             / params[crit].ds_sigma[3]);
-#             }
-#
-#             return 0.0;
-#         }
-#
-#         int getEffectMembership(int crit1, int crit2) {
-#             if ((crit1 == 0 && crit2 == 0) ||
-#                 (crit1 == 0 && crit2 == 1) ||
-#                 (crit1 == 1 && crit2 == 0))
-#                 return 0;
-#             if ((crit1 == 1 && crit2 == 1) ||
-#                 (crit1 == 0 && crit2 == 2) ||
-#                 (crit1 == 2 && crit2 == 0))
-#                 return 1;
-#             if ((crit1 == 1 && crit2 == 2) ||
-#                 (crit1 == 2 && crit2 == 1) ||
-#                 (crit1 == 2 && crit2 == 2))
-#                 return 2;
-#
-#             return 0;
-#         }
-#     public:
-#         GaussExpert(int alts_amount, int crit_amount,
-#                     gauss_parametrs *params_, double **grades_) { // АРГУМЕНТ ДОЛЖЕН БЫТЬ КОНСТАНТНЫМ!!!
-#             crits = crit_amount;
-#             alts = alts_amount;
-#             params = std::move(params_);
-#             grades = std::move(grades_);
-#
-#             calculateParams();
-#
-#             for (int i = 0; i < crits; i++) {
-#                 for (int j = 0; j < 4; j++) {
-#                     std::cout << "Sigma: " << params[i].ds_sigma[j] << std::endl;
-#                 }
-#                 for (int j = 0; j < 4; j++) {
-#                     std::cout << "Y: " << params[i].y[j] << std::endl;
-#                 }
-#             }
-#         }
-#         ~GaussExpert() {
-#             delete[] params;
-#
-#             for (int i = 0; i < alts; i++)
-#                 delete[] grades[i];
-#
-#             delete[] grades;
-#         }
-#
-#         void getBestAlternative() {
-#             double ***fuzzy_grades = new double**[crits];
-#
-#             for (int i = 0; i < crits; i++) {
-#                 fuzzy_grades[i] = new double*[alts];
-#                 for (int j = 0; j < alts; j++)
-#                     fuzzy_grades[i][j] = new double[3];
-#             }
-#
-#
-#             for (int i = 0; i < crits; i++) {
-#                 std::cout << "\n\nCrit #" << i+1 << ":\n";
-#                 for (int j = 0; j < alts; j++) {
-#                     for (int k = 0; k < 3; k++) {
-#                         fuzzy_grades[i][j][k] = gaussEstimate(i, j, k);
-#                         std::cout << std::setw(20) << fuzzy_grades[i][j][k];
-#                     }
-#                  std::cout << "\n";
-#                 }
-#             }
-#
-#
-#
-#
-#             double **accum_table = new double*[alts];
-#             std::vector<double> compares[alts][3];
-#             int ind = 0;
-#             for (int i = 0; i < alts; i++) {
-#                 accum_table[i] = new double[3];
-#                 for (int c1 = 0; c1 < 3; c1++) {
-#                     for (int c2 = 0; c2 < 3; c2++) {
-#                         ind = getEffectMembership(c1, c2);
-#                         compares[i][ind].push_back(std::min(fuzzy_grades[0][i][c1],
-#                                                                fuzzy_grades[1][i][c2]));
-#                     }
-#                 }
-#
-#                 for (int t = 0; t < 3; t++) {
-#                     accum_table[i][t] = compares[i][t][0];
-#                     for (int k = 0; k < compares[i][t].size(); k++)
-#                         accum_table[i][t] = std::max(accum_table[i][t], compares[i][t][k]);
-#                 }
-#
-#             }
-#
-#             std::cout << "Accumulation table:\n";
-#             for (int i = 0; i < alts; i++) {
-#                 for (int j = 0; j < 3; j++)
-#                     std::cout << std::setw(20) << accum_table[i][j];
-#                 std::cout << "\n";
-#             }
-#
-#
-#             double alts_eff[alts];
-#             std::pair<double, int> best_alt = std::make_pair(-1, -1);
-#
-#             for (int i = 0; i < alts; i++) {
-#                 alts_eff[i] = accum_table[i][0] * 0.1 + accum_table[i][1] * 0.5 + accum_table[i][2] * 0.9;
-#                 std::cout << "Alt #" << i+1 << ": " << alts_eff[i] << std::endl;
-#                 if (alts_eff[i] > best_alt.first)
-#                     best_alt = std::make_pair(alts_eff[i], i);
-#             }
-#
-#             std::cout << "\n\nBest alternative is alternative with #" << best_alt.second+1 << "\n\n";
-#
-#
-#             for (int i = 0; i < crits; i++) {
-#                 for (int j = 0; j < alts; j++)
-#                     delete[] fuzzy_grades[i][j];
-#                 delete[] fuzzy_grades[i];
-#             }
-#
-#             delete[] fuzzy_grades;
-#         }
+                if self.gauss_parametrs[i]['y'][j] > self.gauss_parametrs[i]['dominant_value'][2]:
+                    self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][2]
+
+                if self.gauss_parametrs[i]['y'][j] < self.gauss_parametrs[i]['dominant_value'][0]:
+                    self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][0]
+
+    def gaussEstimate(self, crit, alt, membership):
+        if membership == 0:
+            if self.grades[crit][alt] < self.gauss_parametrs[crit]['dominant_value'][0]:
+                return 1
+
+            if self.grades[crit][alt] > self.gauss_parametrs[crit]['y'][0]:
+                return 0
+
+            return math.exp(-((self.grades[crit][alt] - self.gauss_parametrs[crit]['dominant_value'][0]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][0])
+
+        if membership == 1:
+            if (self.gauss_parametrs[crit]['y'][1] <= self.grades[crit][alt]) and \
+               (self.grades[crit][alt] >= self.gauss_parametrs[crit]['y'][2]):
+                return 0;
+
+            if (self.grades[crit][alt] > self.gauss_parametrs[crit]['y'][1]) and \
+                (self.grades[crit][alt] < self.gauss_parametrs[crit]['dominant_value'][1]):
+                return math.exp(-((self.grades[crit][alt]
+                                    - self.gauss_parametrs[crit]['dominant_value'][1]) ** 2)
+                                / self.gauss_parametrs[crit]['ds_sigma'][1])
+
+            return math.exp(-((self.grades[crit][alt]
+                                - self.gauss_parametrs[crit]['dominant_value'][1]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][2]);
+
+        if membership == 2:
+            if self.grades[crit][alt] <= self.gauss_parametrs[crit]['y'][3]:
+                return 0
+
+            if self.grades[crit][alt] >= self.gauss_parametrs[crit]['dominant_value'][2]:
+                return 1
+
+            return math.exp(-((self.grades[crit][alt]
+                                - self.gauss_parametrs[crit]['dominant_value'][2]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][3])
+        return 0.0
+
+    def getEffectMembership(self, crit1, crit2):
+        if ((crit1 == 0 and crit2 == 0) or
+            (crit1 == 0 and crit2 == 1) or
+            (crit1 == 1 and crit2 == 0)):
+            return 0
+
+        if ((crit1 == 1 and crit2 == 1) or
+            (crit1 == 0 and crit2 == 2) or
+            (crit1 == 2 and crit2 == 0)):
+            return 1
+
+        if ((crit1 == 1 and crit2 == 2) or
+            (crit1 == 2 and crit2 == 1) or
+            (crit1 == 2 and crit2 == 2)):
+            return 2
+
+        return 0
+
+    def getBestAlternative(self):
+        #self.grades = [[0] * self.__alts for i in range(2)]
+        fuzzy_grades = [[[0 for k in range(3)] for j in range(self.__alts)] for i in range(self.__crits)]
+        print(fuzzy_grades)
+        #double ***fuzzy_grades = new double**[crits];
+
+       # for i in range(self.__crits):
+       #     fuzzy_grades[i] = [] * self.__alts
+       #     for j in range(self.__alts):
+       #         fuzzy_grades[i][j] = [] * 3
+
+
+        for i in range(self.__crits):
+            #std::cout << "\n\nCrit #" << i+1 << ":\n";
+            for j in range(self.__alts):
+                for k in range(3):
+                    fuzzy_grades[i][j][k] = self.gaussEstimate(i, j, k);
+                    #std::cout << std::setw(20) << fuzzy_grades[i][j][k];
+                #}
+                #std::cout << "\n";
+            #}
+        #}
+        print(fuzzy_grades)
+
+
+        accum_table = [[0] * 3 for i in range(self.__alts)]
+        print("accum_table:", accum_table)
+        #fuzzy_grades = [[[0 for k in range(3)] for j in range(self.__alts)] for i in range(self.__crits)]
+        compares = [[[0] for k in range(3)] for j in range(self.__alts)]
+        print("compares: ", compares)
+        #double **accum_table = new double*[alts];
+        #std::vector<double> compares[alts][3];
+        ind = 0
+
+        for i in range(self.__alts):
+            for c1 in range(3):
+                for c2 in range(3):
+                    ind = self.getEffectMembership(c1, c2);
+                    compares[i][ind].append(min(fuzzy_grades[0][i][c1], fuzzy_grades[1][i][c2]))
+
+            for t in range(3):
+                accum_table[i][t] = compares[i][t][0];
+                for k in range(len(compares[i][t])):
+                    accum_table[i][t] = max(accum_table[i][t], compares[i][t][k])
+
+            # std::cout << "Accumulation table:\n";
+            # for (int i = 0; i < alts; i++) {
+            #     for (int j = 0; j < 3; j++)
+            #         std::cout << std::setw(20) << accum_table[i][j];
+            #     std::cout << "\n";
+            # }
+        print(accum_table)
+
+        alts_eff = [0 for i in range(self.__alts)]
+            #double alts_eff[alts];
+        best_alt = [0.0, 0]
+            #std::pair<double, int> best_alt = std::make_pair(-1, -1);
+
+        for i in range(self.__alts):
+            alts_eff[i] = accum_table[i][0] * 0.1 + accum_table[i][1] * 0.5 + accum_table[i][2] * 0.9;
+                #std::cout << "Alt #" << i+1 << ": " << alts_eff[i] << std::endl;
+            if alts_eff[i] > best_alt[0]:
+                best_alt = [alts_eff[i], i]
+
+
+        print("alts effectivness: ", alts_eff)
+            #std::cout << "\n\nBest alternative is alternative with #" << best_alt.second+1 << "\n\n";
+        print("Best alt#", best_alt[1]+1)
 #
 #
 # };
@@ -268,3 +266,8 @@ class GaussExpert:
 #    system("pause");
 #     return 0;
 # }
+
+
+ge = GaussExpert()
+ge.calculateParams()
+ge.getBestAlternative()
