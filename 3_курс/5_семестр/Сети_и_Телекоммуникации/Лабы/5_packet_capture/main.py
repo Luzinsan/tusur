f979eb9b83eabb5372465a6b09bddeb7a6e3e8de
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-12-12 16:10:24 +0700

    СиТ (Лабы): попытка разобраться в перехвате пакетов - всё плохо

--- 3_курс/5_семестр/Сети_и_Телекоммуникации/Лабы/5_packet_capture/main.py
+++ 3_курс/5_семестр/Сети_и_Телекоммуникации/Лабы/5_packet_capture/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+from scapy.all import *
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+import time
+import binascii
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# будем отправлять данные сами себе
+ip, mac = '192.168.43.44', '00:F4:8D:EF:72:8D'
+# dst_port – порт получателя, src_port – порт отправителя
+dst_port, src_port = 11111, 11110
+# packet_count - количество отправляемых пакетов, n - счетчик пакетов
+packet_count, n = 1000, 1
+# Формирование тестовых данных
+gen_data = [f'{i:02x}' for i in range(1, 129)]
+gen_data_str = ''.join(gen_data)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while n <= packet_count:
+    # Добавление текущего времени в начало паекты данных
+    curr_t = time.localtime()
+    h, m, s = curr_t.tm_hour, curr_t.tm_min, curr_t.tm_sec
+    # Формирование итоговых данных для отправки
+    data = binascii.unhexlify(f'{h:02x}{m:02x}{s:02x}{gen_data_str}')
+    # Формирование и отправка UDP-пакета
+    sendp(Ether(dst=mac)/
+          IP(dst=ip)/
+          UDP(sport=src_port, dport=dst_port, chksum=0x0000)/
+          data)
+    # Задержка на 0.1 сек
+    time.sleep(0.1)
+    n += 1
