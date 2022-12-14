49c78359c50692da9b341508eb1e8bac6c551340
luzinsanHuaweiKubuntu <luzinsan.a.430-2@e.tusur.ru>
2022-12-14 15:50:49 +0700

    СиТ (Лаб4): маршрутизация приложения

--- 3_курс/5_семестр/Сети_и_Телекоммуникации/Лабы/Websockets/Websockets/urls.py
+++ 3_курс/5_семестр/Сети_и_Телекоммуникации/Лабы/Websockets/Websockets/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from minimization import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^results', views.results, name='results'),
+    re_path(r'^minimization', views.minimization, name='minimization'),
+    re_path(r'^user', views.user, name='user'),
+    re_path(r'^$', views.index, name='home'),
 ]
