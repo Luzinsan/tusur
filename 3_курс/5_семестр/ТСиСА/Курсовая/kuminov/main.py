2a215aa2b6905aa8b5e730e14722d41d39d973d4
DarkMessiah5 <pavekum@mail.ru>
2022-12-15 13:13:45 +0300

    Unite GUI and calcualtions; Add rounding
    Add rounding to 2 decimal places.

--- 3_курс/5_семестр/ТСиСА/Курсовая/kuminov/main.py
+++ 3_курс/5_семестр/ТСиСА/Курсовая/kuminov/main.py
@@ -1,7 +1,143 @@
-from gauss_expert import *
+import math
+import dearpygui.dearpygui as dpg
+
+
+class GaussExpert:
+    crits = 2
+    alts = 2
+    grades = []
+    fuzzy_grades = []
+    accum_table = []
+    alts_eff = []
+    best_alt = []
+
+    gauss_parametrs = [{'dominant_value': [0.0] * 3,
+                        'bound_neighb': [0.0] * 2,
+                        'membership_deg': [0.0] * 2,
+                        'ds_sigma': [0.0] * 4,
+                        'y': [0.0] * 4},
+                       {'dominant_value': [0.0] * 3,
+                        'bound_neighb': [0.0] * 2,
+                        'membership_deg': [0.0] * 2,
+                        'ds_sigma': [0.0] * 4,
+                        'y': [0.0] * 4}
+                       ]
+
+    def __init__(self):
+        print(self.grades)
+
+    def calculateParams(self):
+        for i in range(self.crits):
+            dv_ind, bn_ind, md_ind = 0, 0, 0
+            for j in range(4):
+                match j:
+                    case 1:
+                        dv_ind = 1
+                    case 2:
+                        bn_ind = 1
+                        md_ind = 1
+                    case 3:
+                        dv_ind = 2
+                self.gauss_parametrs[i]['ds_sigma'][j] = ((self.gauss_parametrs[i]['bound_neighb'][bn_ind] -
+                                                           self.gauss_parametrs[i]['dominant_value'][dv_ind]) ** 2) \
+                                                         / (-math.log(
+                    self.gauss_parametrs[i]['membership_deg'][md_ind]))
+
+                self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][dv_ind] + (-1) ** j * \
+                                                  math.fabs(
+                                                      (-self.gauss_parametrs[i]['ds_sigma'][j] * math.log(0.05)) ** 0.5)
+
+                if self.gauss_parametrs[i]['y'][j] > self.gauss_parametrs[i]['dominant_value'][2]:
+                    self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][2]
+
+                if self.gauss_parametrs[i]['y'][j] < self.gauss_parametrs[i]['dominant_value'][0]:
+                    self.gauss_parametrs[i]['y'][j] = self.gauss_parametrs[i]['dominant_value'][0]
+
+    def gaussEstimate(self, crit, alt, membership):
+        if membership == 0:
+            if self.grades[crit][alt] < self.gauss_parametrs[crit]['dominant_value'][0]:
+                return 1
+
+            if self.grades[crit][alt] > self.gauss_parametrs[crit]['y'][0]:
+                return 0
+
+            return math.exp(-((self.grades[crit][alt] - self.gauss_parametrs[crit]['dominant_value'][0]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][0])
+
+        if membership == 1:
+            if (self.gauss_parametrs[crit]['y'][1] <= self.grades[crit][alt]) and \
+                    (self.grades[crit][alt] >= self.gauss_parametrs[crit]['y'][2]):
+                return 0
+
+            if (self.grades[crit][alt] > self.gauss_parametrs[crit]['y'][1]) and \
+                    (self.grades[crit][alt] < self.gauss_parametrs[crit]['dominant_value'][1]):
+                return math.exp(-((self.grades[crit][alt]
+                                   - self.gauss_parametrs[crit]['dominant_value'][1]) ** 2)
+                                / self.gauss_parametrs[crit]['ds_sigma'][1])
+
+            return math.exp(-((self.grades[crit][alt]
+                               - self.gauss_parametrs[crit]['dominant_value'][1]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][2])
+
+        if membership == 2:
+            if self.grades[crit][alt] <= self.gauss_parametrs[crit]['y'][3]:
+                return 0
+
+            if self.grades[crit][alt] >= self.gauss_parametrs[crit]['dominant_value'][2]:
+                return 1
+
+            return math.exp(-((self.grades[crit][alt]
+                               - self.gauss_parametrs[crit]['dominant_value'][2]) ** 2)
+                            / self.gauss_parametrs[crit]['ds_sigma'][3])
+        return 0.0
+
+    def getEffectMembership(self, crit1, crit2):
+        if ((crit1 == 0 and crit2 == 0) or
+                (crit1 == 0 and crit2 == 1) or
+                (crit1 == 1 and crit2 == 0)):
+            return 0
+
+        if ((crit1 == 1 and crit2 == 1) or
+                (crit1 == 0 and crit2 == 2) or
+                (crit1 == 2 and crit2 == 0)):
+            return 1
+
+        if ((crit1 == 1 and crit2 == 2) or
+                (crit1 == 2 and crit2 == 1) or
+                (crit1 == 2 and crit2 == 2)):
+            return 2
+
+        return 0
+
+    def getBestAlternative(self):
+        self.fuzzy_grades = [[[0 for k in range(3)] for j in range(self.alts)] for i in range(self.crits)]
+        for i in range(self.crits):
+            for j in range(self.alts):
+                for k in range(3):
+                    self.fuzzy_grades[i][j][k] = round(self.gaussEstimate(i, j, k), 2)
+        self.accum_table = [[0] * 3 for i in range(self.alts)]
+        compares = [[[0] for k in range(3)] for j in range(self.alts)]
+        ind = 0
+        for i in range(self.alts):
+            for c1 in range(3):
+                for c2 in range(3):
+                    ind = self.getEffectMembership(c1, c2);
+                    compares[i][ind].append(min(self.fuzzy_grades[0][i][c1], self.fuzzy_grades[1][i][c2]))
+            for t in range(3):
+                self.accum_table[i][t] = compares[i][t][0];
+                for k in range(len(compares[i][t])):
+                    self.accum_table[i][t] = max(self.accum_table[i][t], compares[i][t][k])
+        self.alts_eff = [0 for i in range(self.alts)]
+        self.best_alt = [0.0, 0]
+        for i in range(self.alts):
+            self.alts_eff[i] = round(self.accum_table[i][0] * 0.1 + self.accum_table[i][1] * 0.5 + self.accum_table[i][
+                2] * 0.9, 2)
+            if self.alts_eff[i] > self.best_alt[0]:
+                self.best_alt = [self.alts_eff[i], i]
+
 
 WIDTH = 960
-HEIGHT = 540
+HEIGHT = 620
 CENTER = int(WIDTH / 2) - 100
 
 dpg.create_context()
@@ -12,7 +148,15 @@ print(ge.gauss_parametrs)
 # region WINDOW#1: Simple Decision Support System
 with dpg.window(label="Simple Decision Support System", tag="Main"):
     # Справочная информация
-    dpg.add_child_window(tag='info', height=380)
+    #with dpg.add_child_window(tag='info', height=380, label='ЫОЫАВОАВЫО'):
+    dpg.add_text(default_value='Система позволяет найти наилучшую альтернативу из n предложенных'
+                               'и оцененных по двум \nкритериям.'
+                               '\nДля выбора альтернативы изпользуется нечеткое многокритериальное'
+                               'оценивание на основе \nГауссовой функции принадлежности.'
+                               '\nAy1 < Ay2 < Ay3 - Доминирующее значение y нечеткого множества,'
+                               'описываюзего терм, ai'
+                               '\nAy1 < j1 < Ay2; Ay2 < j2 <Ay3 - пограничные значения соседних термов'
+                               '\n0 < M1, M2 < 1 - степени принадлежности пограничных значений')
     # Ввод альтернатив
     with dpg.group(horizontal=True):
         dpg.add_text(default_value='Количество альтернатив: ')
@@ -208,7 +352,7 @@ def output_res():
 
 
 with dpg.window(label="Gauss Criteria Grade", tag="window_gauss_grade", width=WIDTH, height=HEIGHT,
-                show=False, no_move=True, no_resize=True, popup=True):
+                show=False, no_move=True, no_resize=True, popup=False):
     # Таблицы оценок по критериям
     for crit in range(ge.crits):
         dpg.add_table(tag=f'table_membership_crit{crit + 1}',
@@ -222,7 +366,7 @@ with dpg.window(label="Gauss Criteria Grade", tag="window_gauss_grade", width=WI
 # endregion
 
 # region WINDOW#5:
-with dpg.window(label='Results', tag="window_result", width=WIDTH, height=HEIGHT,
+with dpg.window(label='Results', modal=True, tag="window_result", width=WIDTH, height=HEIGHT,
                 show=False, no_move=True, no_resize=True):
     dpg.add_table(tag='table_result',
                   resizable=True, policy=dpg.mvTable_SizingStretchProp,
@@ -243,26 +387,39 @@ def on_exit(sender, app_data, user_data):
 
 # Оформление
 with dpg.theme() as global_theme:
-    # with dpg.theme_component(dpg.mvAll):
-    #     dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (5, 7, 143), category=dpg.mvThemeCat_Core)
-    #     dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (12, 90, 160), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
     with dpg.theme_component(dpg.mvInputInt):
-        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (30, 77, 180), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (12, 90, 160), category=dpg.mvThemeCat_Core)
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
     with dpg.theme_component(dpg.mvInputText):
-        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (30, 177, 180), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (12, 90, 160), category=dpg.mvThemeCat_Core)
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
     with dpg.theme_component(dpg.mvText):
-        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (15, 161, 131), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (12, 90, 160), category=dpg.mvThemeCat_Core)
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
 dpg.bind_theme(global_theme)
 
+big_let_start = 0x00C0  # Capital "A" in cyrillic alphabet
+big_let_end = 0x00DF  # Capital "Я" in cyrillic alphabet
+small_let_end = 0x00FF  # small "я" in cyrillic alphabet
+remap_big_let = 0x0410  # Starting number for remapped cyrillic alphabet
+alph_len = big_let_end - big_let_start + 1  # adds the shift from big letters to small
+alph_shift = remap_big_let - big_let_start  # adds the shift from remapped to non-remapped
+
 with dpg.font_registry():
-    with dpg.font('C:\Windows\Fonts\CALIBRIL.TTF', 16, default_font=True, id="Default font"):
+    with dpg.font(r"c:\windows\fonts\times.ttf", 24, default_font=True, id="Default font"):
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+        biglet = remap_big_let  # Starting number for remapped cyrillic alphabet
+        for i1 in range(big_let_start, big_let_end + 1):  # Cycle through big letters in cyrillic alphabet
+            dpg.add_char_remap(i1, biglet)  # Remap the big cyrillic letter
+            dpg.add_char_remap(i1 + alph_len, biglet + alph_len)  # Remap the small cyrillic letter
+            biglet += 1  # choose next letter
 dpg.bind_font("Default font")
 ########################################################################################################################
-dpg.create_viewport(title='НЕЧЁТКОЕ МНОГОКРИТЕРИАЛЬНОЕ ОЦЕНИВАНИЕ МЕТОДОМ ГАУССА', width=WIDTH, height=HEIGHT,
+dpg.create_viewport(title='Simple Decision Support System', width=WIDTH, height=HEIGHT,
                     resizable=False)
 dpg.set_global_font_scale(1)
 dpg.set_exit_callback(on_exit)
