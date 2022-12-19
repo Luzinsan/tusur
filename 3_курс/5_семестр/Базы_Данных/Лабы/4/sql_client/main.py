33eee2686a4e90a0c85f40f518e23300b648a1fe
luzinsan <luzinsan.a.430-2@e.tusur.ru>
2022-12-19 23:52:51 +0700

    БД (Лабы): 4лр - вывод списка таблиц и списка полей каждой таблицы

--- 3_курс/5_семестр/Базы_Данных/Лабы/4/sql_client/main.py
+++ 3_курс/5_семестр/Базы_Данных/Лабы/4/sql_client/main.py
@@ -1,6 +1,7 @@
 from settings import *
 
 
+
 def insert_record():
     pass
 
@@ -30,6 +31,31 @@ def output_request(cursor):
     print(cursor.fetchall())
 
 
+def output_columns(sender, table_name):
+    connection, cursor = dpg.get_item_user_data('auth')
+    dpg.delete_item('output_columns_error')
+    try:
+        request = f"SELECT column_name FROM information_schema.columns WHERE table_name='{table_name}';"
+        cursor.execute(request)
+        list_columns = [column[0] for column in cursor.fetchall()]
+        dpg.configure_item('list_columns', items=list_columns, show=True, num_items=len(list_columns))
+        print(f"Поля таблицы: {list_columns}")
+    except (Exception, Error) as error:
+        dpg.add_text(tag='output_columns_error', default_value=Error, color=(255, 0, 0), before='list_columns')
+
+
+def output_tables(cursor):
+    dpg.delete_item('output_list_error')
+    try:
+        request = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
+        cursor.execute(request)
+        tables = [table[0] for table in cursor.fetchall()]
+        dpg.configure_item('list_tables', items=tables, show=True, num_items=len(tables))
+        print(f"Таблицы в базе данных: {tables}")
+    except (Exception, Error) as error:
+        dpg.add_text(tag='output_list_error', default_value=Error, color=(255, 0, 0), before='list_tables')
+
+
 def connect_database():
     dpg.delete_item('connect_error')
     dpg.delete_item('connect_success')
@@ -40,17 +66,19 @@ def connect_database():
                  'database': dpg.get_value('database')}
     try:
         # Подключение к существующей базе данных с использованием контекстного менеджера
-        with closing(psycopg2.connect(user=auth_data['user'],
+        connection = psycopg2.connect(user=auth_data['user'],
                                       password=auth_data['password'],
                                       host=auth_data['host'],
                                       port=auth_data['port'],
-                                      database=auth_data['database'])) as connection:
-            # Распечатать сведения о PostgreSQL
-            print(connection.get_dsn_parameters())
-            # Инициализация курсора для выполнения операций с базой данных
-            cursor = connection.cursor(cursor_factory=DictCursor)
-            dpg.add_text(tag='connect_success', before='send_auth', color=(0, 255, 0), default_value=f"База данных успешно подключена.")
-            dpg.set_item_user_data('auth', [connection, cursor])
+                                      database=auth_data['database'])
+        # Распечатать сведения о PostgreSQL
+        print(connection.get_dsn_parameters())
+
+        # Инициализация курсора для выполнения операций с базой данных
+        cursor = connection.cursor(cursor_factory=DictCursor)
+        dpg.add_text(tag='connect_success', before='send_auth', color=(0, 255, 0), default_value=f"База данных успешно подключена.")
+        dpg.set_item_user_data('auth', [connection, cursor])
+        output_tables(cursor)
     except (Exception, Error) as error:
         dpg.add_text(tag='connect_error', before='send_auth', color=(255, 0, 0), default_value=f"Ошибка при работе с PostgreSQL: {error}")
 
@@ -72,6 +100,9 @@ with dpg.window(label="AUTHORIZATION", modal=True, show=False, tag="auth", no_ti
 with dpg.window(label="Main", tag="Main"):
     with dpg.menu_bar():
         dpg.add_menu_item(label="Log in", callback=lambda: dpg.configure_item("auth", show=True))
+    dpg.add_listbox(tag='list_tables', callback=output_columns)
+    dpg.add_separator()
+    dpg.add_listbox(tag='list_columns')
     dpg.add_separator()
 dpg.set_primary_window("Main", True)
 
