bc5f98fe482265b8e593f269f71d90d263106964
luzinsan-archHu <luzinsan@mail.ru>
2023-09-12 19:54:08 +0700

    [ИБ]: считывание алфавита из файла, поиск алфавита через регулярки, коррекция отчета

--- 3_курс/7_semester/ИБ/Лабораторные/1/lr1.py
+++ 3_курс/7_semester/ИБ/Лабораторные/1/lr1.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
 import time
+import re
 
 
 def set_path(sender, app_data):
@@ -16,10 +17,10 @@ with dpg.file_dialog(directory_selector=False, show=False, callback=set_path, ta
     dpg.add_file_extension(".txt", color=(0, 255, 0, 255), custom_text="[Text]")
 
 
-def decrypting(input_data, shift):
+def decrypting(input_data, shift, area_alphabetic):
     decrypted_data = []
     for row in input_data:
-        decrypted_data.append(''.join(encrypting_row(row, -shift)))
+        decrypted_data.append(''.join(encrypting_row(row, -shift, area_alphabetic)))
     return decrypted_data
 
 
@@ -37,29 +38,34 @@ def get_input_data():
     return input_data, is_multiline
 
 
-def encrypting_row(row, shift):
-    cyrillic_lower = [chr(symbol) for symbol in range(ord('а'), ord('я') + 1)]
-    cyrillic = [chr(symbol) for symbol in range(ord('А'), ord('Я') + 1)]
-    latin_lower = [chr(symbol) for symbol in range(ord('a'), ord('z') + 1)]
-    latin = [chr(symbol) for symbol in range(ord('A'), ord('Z') + 1)]
+def get_alphabet_area():
+    falphabets = open('alphabetic.txt', 'r', encoding="utf-8")
+    alphabet_area = falphabets.readlines()
+    alphabets = {}
+    for item in alphabet_area:
+        item = item[:3]
+        alphabets[item] = tuple(chr(symbol)
+                                for symbol in range(ord(item[0]), ord(item[-1]) + 1))
+    falphabets.close()
+    return alphabets
+
+
+def encrypting_row(row, shift, area_alphabetic):
+    combined_pattern = [f'[{pattern}]' for pattern in area_alphabetic.keys()]
     sign = -1 if shift < 0 else 1
     shift = abs(shift)
     enc_row = []
     for symbol in row:
-        if symbol in cyrillic_lower + cyrillic + latin_lower + latin:
-            if symbol in cyrillic_lower:
-                alphabetic = cyrillic_lower
-            elif symbol in cyrillic:
-                alphabetic = cyrillic
-            elif symbol in latin_lower:
-                alphabetic = latin_lower
-            elif symbol in latin:
-                alphabetic = latin
+        index_alphabetic = ''.join([pattern[1:-1]
+                            for pattern in combined_pattern
+                            if re.search(pattern, symbol)])
+        if index_alphabetic:
+            alphabetic = area_alphabetic[index_alphabetic]
             shift_for_symbol = (shift % (len(alphabetic) - 1)) * sign
             index = (alphabetic.index(symbol) + shift_for_symbol) % (len(alphabetic))
             enc_row.append(alphabetic[index])
         else:
-            enc_row.append(chr(ord(symbol) + (shift * sign)))
+            enc_row.append(symbol)
     return enc_row
 
 
@@ -72,20 +78,22 @@ def encrypting(sender, app_data, user_data):
     encrypted_data = []
     num_dots = 1
     dpg.configure_item('dots', color=(0, 0, 255, 255))
-    fout = open('output.txt', 'a', encoding="utf-8")
+    area_alphabetic = get_alphabet_area()
     if type(input_data) is list:
         for row in input_data:
             time.sleep(0.1)
-            encrypted_data.append(''.join(encrypting_row(row, shift)))
+            encrypted_data.append(''.join(encrypting_row(row, shift, area_alphabetic)))
             dpg.set_value('dots', value='.  ' * num_dots)
             num_dots += 1
     else:
         encrypted_data = is_multiline.join(encrypting_row(input_data, shift))
 
     dpg.set_value('encrypted', value=is_multiline.join(encrypted_data))
+    fout = open('output.txt', 'a', encoding="utf-8")
     fout.writelines(is_multiline.join(encrypted_data) + '\n')
+    fout.close()
     # region test
-    test = decrypting(encrypted_data, shift)
+    test = decrypting(encrypted_data, shift, area_alphabetic)
     dpg.set_value('test', value=is_multiline.join(test))
     if ''.join(input_data) == ''.join(test):
         dpg.configure_item('dots', default_value='True', color=(0, 255, 0, 255))
@@ -127,4 +135,3 @@ with dpg.window(label="Лабораторная работа #1", tag='lr1', sho
         dpg.add_text(tag='test', label='Test', show_label=True)
         dpg.add_separator()
         dpg.add_text(tag='dots', color=(0, 0, 255, 255))
-
