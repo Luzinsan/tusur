9dc25b0cf617585c89040b8cf8c37ba7b33ebb85
luzinsan <luzinsan.a.430-2@e.tusur.ru>
2023-10-18 12:58:47 +0700

    [ПП]: почти сделанный отчёт по lr4

--- 4 курс/7_semester/ПП/labs/proj_all/src/openLab.cpp
+++ 4 курс/7_semester/ПП/labs/proj_all/src/openLab.cpp
@@ -1,13 +1,19 @@
 #include <omp.h>
 #include <iostream>
 #include <math.h>
+#include <fstream>
+#define CHUNK 1
 
 static double f(double a, double c);
 static double fi(double a, double c);
 
 void lab4(int argc, char *argv[])
 {
-    int intervals = 12;
+    std::ofstream fout("/home/luzinsan/TUSUR_learn/4 курс/7_semester/ПП/labs/proj_all/bin/4/output.txt",
+                        std::ios::app);
+    fout << "\n\n-------------------------------------------------------------";
+    int intervals = 100000000;
+    fout << "\nNumber of intervals: " << intervals;
     // std::cout << "\nEnter the number of intervals (0 quit): ";
     // std::cin >> intervals;
     if (!intervals) return;
@@ -21,31 +27,36 @@ void lab4(int argc, char *argv[])
     // std::cin >> c;
 
     int max_threads = omp_get_max_threads();
-    std::cout << "\nMaximum number of threads: " << max_threads;
-    
-    omp_set_num_threads(std::stoi(argv[1]));
+    fout << "\nMaximum number of threads: " << max_threads;
+    if (argv[1])
+        omp_set_num_threads(std::stoi(argv[1]));
     #pragma omp parallel
     #pragma omp master
-        std::cout << "\nNumber of threads: " << omp_get_num_threads();
+        fout << "\nEvaluation on " << omp_get_num_threads() << " threads";
     
     double integral = 0;
     double step = (xh - xl) / static_cast<double>(intervals);
-    std::cout << "\nStep: " << step << '\n';
+    fout << "\nStep: " << step << '\n';
     double startwtime = omp_get_wtime();
+    fout << "\nSTATIC chunk:" << CHUNK;
+    // fout << "\nSTATIC auto";
+    // fout << "\nDYNAMIC chunk:" << CHUNK;
+    // fout << "\nDYNAMIC auto";
+
     #pragma omp parallel for shared(step, xl, c) reduction (+: integral)\
-                schedule(dynamic) 
+                schedule(static, CHUNK) 
         for (int i = 1; i <= intervals; i++)
         {
             double x = xl + step * ((double)i - 0.5);
             integral += f(x, c) * step;
-            #pragma omp critical
-                std::cout << omp_get_thread_num()
-                          << ": " << "x=" << x << ";\t f(x)=" << f(x, c) 
-                          << "; \tpart of integral=" << integral << '\n';
+            // #pragma omp critical
+            //     std::cout << omp_get_thread_num()
+            //               << ": " << "x=" << x << ";\t f(x)=" << f(x, c) 
+            //               << "; \tpart of integral=" << integral << '\n';
         }
-    std::cout << std::scientific << "\nIntegral is approximately: " << integral;
-    std::cout << std::scientific << "\nError: " << integral - fi(xh, c) + fi(xl, c);
-    std::cout << std::scientific << "\nTime of calculation: " << omp_get_wtime() - startwtime;
+    fout << std::scientific << "\nIntegral is approximately: " << integral;
+    fout << std::scientific << "\nError: " << integral - fi(xh, c) + fi(xl, c);
+    fout << std::scientific << "\nTime of calculation: " << omp_get_wtime() - startwtime;
 }
 
 int main(int argc, char *argv[])
