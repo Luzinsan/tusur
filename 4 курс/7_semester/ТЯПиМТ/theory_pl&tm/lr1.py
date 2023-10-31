373cf15de11194086e5f00a26e3e8f4b5202755e
luzinsan <luzinsan.a.430-2@e.tusur.ru>
2023-10-31 10:07:46 +0700

    [ТЯПиМТ]: внедрение действий, правки

--- 4 курс/7_semester/ТЯПиМТ/theory_pl&tm/lr1.py
+++ 4 курс/7_semester/ТЯПиМТ/theory_pl&tm/lr1.py
@@ -1,8 +1,8 @@
-import re
-
 import pandas as pd
 import dearpygui.dearpygui as dpg
+import re
 from __init__ import initialize
+
 pd.set_option('display.max_columns', None)
 
 
@@ -18,8 +18,22 @@ class DFSM:
                  file_transition: str = 'transition.ods'):
         self.__DELTA__ = pd.read_excel(file_transition, index_col=0, engine="odf", dtype=str)
         self.__types__ = '|'.join(self.__DELTA__.columns[:5])
+        self.__container__ = set(self.__types__.split('|'))
         self.__header__ = re.compile('(' + ')|('.join([str(column) for column in self.__DELTA__.columns]) + ')')
         print(f'Функция переходов: \n{self.__DELTA__}\n\n')
+
+    def func(self, _func: int, row: int, column: int, buffer: str, symbol: str) -> str:
+        match _func:
+            case 1:
+                buffer += symbol
+            case 2:
+                if buffer in self.__container__:
+                    raise ValueError(f'Repeated id on the row={row}\tcolumn={column}'
+                                     f'\nid={buffer}'
+                                     f'\nfor container:{self.__container__ - set(self.__types__.split("|"))}')
+                self.__container__ |= {buffer}
+                buffer = ''
+        return buffer
 
     def analyze(self, input_string: str):
         q = 0
@@ -35,55 +49,35 @@ class DFSM:
             if symbol == '\n':
                 column = 0
                 row += 1
-            else:
-                column += 1
             print(f"current string: {input_string[index:]}")
             match = re.match(self.__header__, input_string[index:])
             if match:
                 print('match: ', match.groups())
-                len_shift = len(match.group())
+                symbol = match.group()
+                len_shift = len(symbol)
                 match = [i for i, val in enumerate(match.groups()) if val is not None][0]
                 print('column: ', self.__DELTA__.columns[match], ' index of col: ', match)
-                try:
-                    print(f'\t=>>>q={q}\tsymbol={symbol}\tcolumn={match}')
-                    data_zp: str = self.__DELTA__.iloc[int(q), int(match)]
+                data_zp = self.__DELTA__.iloc[int(q), int(match)]
+                if data_zp == 'HALT':
+                    self.__container__ -= set(self.__types__)
+                    return True
+                elif type(data_zp) is str:
                     if len(data_zp.split()) == 2:
-                        q, func = data_zp.split()
+                        q, _func = map(int, data_zp.split())
+                        buffer = self.func(_func, row, column, buffer, symbol)
                     else:
-                        q, func = int(data_zp), 0
-                    print(f'\tnext state: q={q}\tfunc={func}')
+                        q = int(data_zp)
                     [next(iterator) for interator in range(len_shift - 1)]
                     index += len_shift
+                    column += len_shift
                     print(f"next index={index}\titerator={iterator}")
-                except KeyError as err:
-                    raise ValueError(f'Invalid type on the row={row}\tcolumn={column}'
-                                     f'\nid={symbol}')
+                else:
+                    raise ValueError(f'Syntax error on the row={row}\tcolumn={column}'
+                                     f'\n{input_string[:-1]}'
+                                     f'\nsymbol={symbol}\tq={q}\tbuffer={buffer}\t\ncontainer={self.__container__ - set(self.__types__.split("|"))}')
             else:
                 raise ValueError(f'Syntax error on the row={row}\tcolumn={column}'
-                                 f'\n{input_string[:-1]}'
-                                 f'\n{" " * len(input_string[:-1])}\r^')
-            #         case self.__HALT__:
-            #             self.__container__ -= set(self.__types__)
-            #             return True
-            #
-            #     except KeyError as err:
-            #         raise ValueError(f'Invalid type on the row={row}\tcolumn={column}'
-            #                          f'\nid={symbol}')
-
-        #     match func:
-        #         case 1:
-        #             buffer += symbol
-        #         case 2:
-        #             if buffer in self.__container__:
-        #                 raise ValueError(f'Repeated id or using reserved name on the row={row}\tcolumn={column}'
-        #                                  f'\nid={buffer}'
-        #                                  f'\nfor container:{self.__container__ - set(self.__types__)}'
-        #                                  f'\nand types: {self.__types__}')
-        #             self.__container__ |= {buffer}
-        #             buffer = ''
-        #
-        #     print(f'symbol={symbol}\tq={q}\tfunc={func}\tbuffer={buffer}\tcontainer={self.__container__ - set(self.__types__)}'
-        #           f'\nanalyzing: row={row}\tcolumn={column}')
+                                 f'\n{input_string[:-1]}')
 
 
 def get_input_data():
